32aa423bdfc24a862ce82d835bb39eaf6b09cd5d
ShelterLu <85344586+ShelterLu@users.noreply.github.com>
2023-07-08 12:37:27 +0800

    555

--- main.go
+++ main.go
@@ -255,7 +255,7 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
+	 sus = append(sus, 315841791)
 	// sus = append(sus, 87654321)
 
 	// 启用 webui
@@ -284,7 +284,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "伊雷娜"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
